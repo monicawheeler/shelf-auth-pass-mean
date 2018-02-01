1f6585b028460b035dd6749ffa0c443456f5b576
Monica Wheeler <monica.c.wheeler@gmail.com>
2018-02-01 15:38:54 -0600

    add get request

--- server/public/scripts/services/user.service.js
+++ server/public/scripts/services/user.service.js
@@ -25,12 +25,30 @@ myApp.service('UserService', ['$http', '$location', function ($http, $location) 
         // user has no session, bounce them back to the login page
         $location.path("/shelf");
       });
-  } 
+  }
+
+  self.getImages = function () {
+    $http.get('/api/user')
+      .then(function (response) {
+          self.userObject.shelfItem = response.data.shelfItem;
+          console.log('User Shelf Item: ', self.userObject.shelfItem);
+      },
+      // error response of unauthorized (403)
+      function(response) {
+        console.log('error:', response);
+        
+      });
+  }
+
+
+  self.getImages();
+
   self.shelfADdog = function (userId, newDdog) {
     console.log('ddog clickin');
     $http.post(`/api/user/ddogger/${userId}`, newDdog) 
     .then(function (response) {
         console.log('successful post response' , response);
+        self.getImages();
     })
     .catch(function (error) {
         console.log('error on post response' , error);
